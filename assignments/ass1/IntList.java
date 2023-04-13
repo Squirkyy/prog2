372cc73cbfbe47ffd9f82f02bc538b7e8dd459ea
Darius Vollmer <dariusv@outlook.de>
2023-04-13 20:21:37 +0200

    finished IntList

--- assignments/ass1/IntList.java
+++ assignments/ass1/IntList.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class IntList {
     public static void main(String[] args) {
-        System.out.println(listToInt(intToList(9826)));
+        testFunction();
     }
 
     public static int countNumbers(int num) {
@@ -27,5 +29,30 @@ public class IntList {
             factor *= 10;
         }
         return num;
+    }
+    public static int[] addOne (int[] numbers) {
+        return intToList(1+listToInt(numbers));
+    }
+
+    private static void testFunction() {
+        int number1 = 5291;
+        int number2 = 9999;
+        int[] number1array = intToList(number1);
+        int[] number2array = intToList(number2);
+        System.out.println("countNumbers: ");
+        System.out.println(number1 + " has the size: " + countNumbers(number1));
+        System.out.println(number2 + " has the size: " + countNumbers(number2));
+        System.out.println("");
+        System.out.println("intToList:");
+        System.out.println(number1 + " is the following as an array: " + Arrays.toString(number1array));
+        System.out.println(number2 + " is the following as an array: " + Arrays.toString(number2array));
+        System.out.println("");
+        System.out.println("listToInt:");
+        System.out.println(Arrays.toString(number1array) + " is the following as an int: " + listToInt(number1array));
+        System.out.println(Arrays.toString(number2array) + " is the following as an int: " + listToInt(number2array));
+        System.out.println("");
+        System.out.println("addOne:");
+        System.out.println(Arrays.toString(number1array) + " is the following when added 1: " + Arrays.toString(addOne(number1array)));
+        System.out.println(Arrays.toString(number2array) + " is the following when added 1: " + Arrays.toString(addOne(number2array)));
     }
 }
