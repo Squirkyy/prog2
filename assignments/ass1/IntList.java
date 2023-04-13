867070ea7a49de11924988439c901d212587f26d
Darius Vollmer <dariusv@outlook.de>
2023-04-13 18:39:47 +0200

    Added c) to IntList

--- assignments/ass1/IntList.java
+++ assignments/ass1/IntList.java
@@ -1,23 +1,31 @@
-import java.util.Arrays;
-
 public class IntList {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(intToList(1241)));
+        System.out.println(listToInt(intToList(9826)));
     }
 
-    public static int countNumbers (int num) {
-        return (int)(Math.log10(num)+1);
+    public static int countNumbers(int num) {
+        return (int) (Math.log10(num) + 1);
     }
 
     public static int[] intToList(int value) {
         int[] list = new int[countNumbers(value)];
-        int i = 0;
+        int i = countNumbers(value) - 1;
         while (value > 0) {
             int remainer = value % 10;
             list[i] = remainer;
-            
+            value = value / 10;
+            i--;
         }
         return list;
     }
 
+    public static int listToInt(int[] list) {
+        int num = 0;
+        int factor = 1;
+        for (int i = list.length - 1; i >= 0; i--) {
+            num += list[i] * factor;
+            factor *= 10;
+        }
+        return num;
+    }
 }
