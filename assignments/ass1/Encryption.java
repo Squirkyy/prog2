508b7af9f4acac54ca655c1f41090c5145c15ae6
Darius Vollmer <dariusv@outlook.de>
2023-04-14 14:27:46 +0200

    finished Encryption.java

--- assignments/ass1/Encryption.java
+++ assignments/ass1/Encryption.java
@@ -1,15 +1,35 @@
 public class Encryption {
-    public static void main (String[] args) {
-        System.out.println("der nächste int lautet: " + nextInt(3, 7, 28, 256));
-        
-        char test[] = {'H', 'A', 'L', 'L', 'O'};
+    public static void main(String[] args) {
+        char[] test = { 'H', 'A', 'L', 'L', 'O' };
+        int[] keys = generateKeys(5, 256);
         printCharArray(test);
+        int[] encrypt = encrypt(test, keys);
+        for (int i : encrypt) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        char[] decrypt = decrypt(encrypt, keys);
+        printCharArray(decrypt);
+
     }
+
     public static int nextInt(int a, int b, int m, int x) {
         return (a * x + b) % m;
     }
 
-    public static void printCharArray (char[] letter) {
+    public static int[] generateKeys(int size, int start) {
+        int[] keys = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (i == 0) {
+                keys[i] = nextInt(3, 7, 28, start);
+            } else {
+                keys[i] = nextInt(3, 7, 28, keys[i - 1]);
+            }
+        }
+        return keys;
+    }
+
+    public static void printCharArray(char[] letter) {
         for (char c : letter) {
             System.out.print(c);
         }
@@ -17,7 +37,18 @@ public class Encryption {
     }
 
     public static int[] encrypt(char[] letters, int[] keys) {
-        
+        int[] encryptArr = new int[letters.length];
+        for (int i = 0; i < letters.length; i++) {
+            encryptArr[i] = (((int) letters[i]) ^ keys[i]);
+        }
+        return encryptArr;
     }
 
+    public static char[] decrypt(int[] letters, int[] keys) {
+        char[] encryptArr = new char[letters.length];
+        for (int i = 0; i < letters.length; i++) {
+            encryptArr[i] = (char) (letters[i] ^ keys[i]);
+        }
+        return encryptArr;
+    }
 }
