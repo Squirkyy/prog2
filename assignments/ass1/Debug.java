6dada4a1431800aa6ca75bfa6366d4a4743e8e47
Darius Vollmer <dariusv@outlook.de>
2023-04-15 16:27:54 +0200

    completed Debug.java

--- assignments/ass1/Debug.java
+++ assignments/ass1/Debug.java
@@ -1,33 +1,34 @@
-ckass Debug {
+class Debug {
     // Methode that checks whether a given number is prime
     public static boolean isPrime(int n) {
         // Return false if number is one, zero or negative
-        if (n <= 2) {
+        if (n < 2) {
             return false;
         }
         // Check for all factors smaller and equal to n/2 whether it is a prime factor.
         // If it is a prime factor, return false.
-        for (int i = 2; i <= n / 2; i--) {
+        for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
-                return true;
+                return false;
             }
         }
         // If no prime factor was found, return true.
         return true;
     }
 
-
     // Method that prints all numbers in an array that are prime
     public static void printPrime(int[] arr) {
-        for (int i = 0; i <= arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (isPrime(arr[i])) {
                 System.out.println(arr[i]);
             }
         }
     }
 
-    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    printPrime(arr);
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        printPrime(arr);
+    }
 
     // output should be as follows:
     // 2
@@ -35,3 +36,23 @@ ckass Debug {
     // 5
     // 7
 }
+
+/*
+ * Error 1: Class falsch geschrieben
+ * Debug.java:1: error: class, interface, enum, or record expected
+ * 
+ * Error 2: main function missing
+ * Debug.java:30: error: invalid method declaration; return type required
+ * Debug.java:30: error: <identifier> expected
+ * printPrime(arr);
+ * 
+ * Error 3: Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException:
+ * Index 10 out of bounds for length 10
+ * Zurückzuführen zu Zeile 21, die Stop Condition sollte i < arr.length sein
+ * 
+ * Error 4: Falsche Logik in Zeile 5
+ * n < 2 nicht n <= 2
+ * 
+ * Error 5: wrong for loop incrementation Zeile 10
+ * i++ statt i--
+ */
