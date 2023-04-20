8704a850bfb6876b84f577ddbf5635662f0df1f7
Darius Vollmer <dariusv@outlook.de>
2023-04-21 00:36:16 +0200

    more testcases needed -> check assignment

--- assignments/ass2/task1/Main.java
+++ assignments/ass2/task1/Main.java
@@ -9,40 +9,28 @@ public class Main {
     }
 
     public static Tree TestTree1() {
-        Tree root = new Tree(1);
-        Tree left = new Tree(2);
-        Tree right = new Tree(3);
-        root.setLeftTree(left);
-        root.setRightTree(right);
-        Tree left_left = new Tree(4);
-        Tree left_right = new Tree(5);
-        left.setLeftTree(left_left);
-        left.setRightTree(left_right);
-        Tree right_left = new Tree(6);
-        Tree right_right = new Tree(7);
-        right.setLeftTree(right_left);
-        right.setRightTree(right_right);
+        Tree root = new Tree(4);
+        Tree leftChild = new Tree(2);
+        Tree rightChild = new Tree(6);
+        root.setLeftTree(leftChild);
+        root.setRightTree(rightChild);
+        leftChild.setLeftTree(new Tree(1));
+        leftChild.setRightTree(new Tree(3));
+        rightChild.setLeftTree(new Tree(5));
+        rightChild.setRightTree(new Tree(7));
         return root;
     }
 
     public static Tree TestTree2() {
         Tree root = new Tree(10);
-        Tree left = new Tree(20);
-        Tree right = new Tree(30);
-        root.setLeftTree(left);
-        root.setRightTree(right);
-        Tree left_left = new Tree(40);
-        Tree left_right = new Tree(50);
-        left.setLeftTree(left_left);
-        left.setRightTree(left_right);
-        Tree right_left = new Tree(60);
-        Tree right_right = new Tree(70);
-        right.setLeftTree(right_left);
-        right.setRightTree(right_right);
-        Tree left_left_left = new Tree(80);
-        left_left.setLeftTree(left_left_left);
-        Tree right_right_left = new Tree(90);
-        right_right.setLeftTree(right_right_left);
+        Tree leftChild = new Tree(5);
+        Tree rightChild = new Tree(15);
+        root.setLeftTree(leftChild);
+        root.setRightTree(rightChild);
+        leftChild.setLeftTree(new Tree(2));
+        leftChild.setRightTree(new Tree(7));
+        rightChild.setLeftTree(new Tree(13));
+        rightChild.setRightTree(new Tree(17));
         return root;
     }
 }
