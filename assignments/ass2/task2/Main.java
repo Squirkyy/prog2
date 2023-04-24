89a25bd4dd08cc8427d142450eb468feef78625a
Darius Vollmer <dariusv@outlook.de>
2023-04-24 11:32:11 +0200

    if i have to work on this fucking assignment one more time im gonna combust

--- assignments/ass2/task2/Main.java
+++ assignments/ass2/task2/Main.java
@@ -8,21 +8,26 @@ class Main {
     Marketplace mp = new Marketplace();
     mp.addUser(User1);
     mp.addUser(user2);
+    System.out.println("Two users:");
     System.out.println(mp.str());
+    Item[] items = User1.getItems();
+    items[0].setPrice(69420);
+    User1.setItems(items);
+    
+    System.out.println("Changed Price");
+    System.out.println(mp.str()); 
+
+    System.out.println("User2 removed");
+    mp.removeUser(user2);
+    System.out.println(mp.str()); 
     }
     //UserCreator with random items
     private static User newUser(String name) {
         User test = new User(name, name + "password");
-        for (int i = 1; i  < randomInt(1, 10); i++) {
-            Item item = new Item(test.getUsername() + i + "item", (float) Math.random()*100, test, "Item #" + i + " of " + test.getUsername());
+        for (int i = 0; i  < 2; i++) {
+            Item item = new Item(test.getUsername() + i + "item", (float) Math.floor(Math.random()*100), test, "Item #" + i + " of " + test.getUsername());
             test.addItem(item);
         }
         return test;
-    } 
-
-    private static int randomInt(int min, int max){
-        int range = (((max - min) + 1));     
-        return (int)(Math.random() * range) + min;
-     }
-
+    }
 }
